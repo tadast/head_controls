e1b92f6643215424fe03f065d8400a074e388c6e
Tadas Tamosauskas <tadas@pdfcv.com>
2013-09-15 15:50:52 +0100

    Move the head back and forth to zoom

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,1 +1,57 @@
-alert "Hi"
+$ ->
+  videoInput = document.getElementById('inputVideo')
+  canvasInput = document.getElementById('inputCanvas')
+  htracker = new headtrackr.Tracker()
+
+  htracker.init(videoInput, canvasInput)
+  new TrackControll(htracker)
+
+class TrackControll
+  constructor: (@tracker) ->
+    @tracker.start()
+    @log = new TrackLog()
+    @article = $("article")
+    $(document).on "facetrackingEvent", (e) =>
+      @log.register(e)
+
+    $(document).on "zoomEvent", (e, zoom) =>
+      @zoom(zoom)
+
+
+  zoom: (diff) ->
+    current_size = parseInt(@article.css('font-size'))
+    new_size = current_size + (diff / 10)
+    new_size = Math.max(5, new_size)
+    @article.css('font-size', "#{new_size}px");
+
+
+class TrackLog
+  constructor: ->
+    @log = []
+    @size = 4
+    @logging_on = false
+    $(document).on "click", =>
+      @logging_on = !@logging_on
+
+  register: (event) ->
+    @push(event)
+    zoomDelta = @last().originalEvent.width - event.originalEvent.width
+    if zoomDelta != 0
+      $(document).trigger "zoomEvent", zoomDelta
+
+  pure: (event) ->
+    width: event.originalEvent.width
+    height: event.originalEvent.height
+    x: event.originalEvent.x
+    y: event.originalEvent.y
+
+  push: (event) ->
+    @log.push(event)
+    if @log.length > @size
+      @log.shift()
+    if @logging_on
+      console.log(@pure(event))
+
+  last: ->
+    @log[0]
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,71 @@
-alert("Hi");
+var TrackControll, TrackLog;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+$(function() {
+  var canvasInput, htracker, videoInput;
+  videoInput = document.getElementById('inputVideo');
+  canvasInput = document.getElementById('inputCanvas');
+  htracker = new headtrackr.Tracker();
+  htracker.init(videoInput, canvasInput);
+  return new TrackControll(htracker);
+});
+TrackControll = (function() {
+  function TrackControll(tracker) {
+    this.tracker = tracker;
+    this.tracker.start();
+    this.log = new TrackLog();
+    this.article = $("article");
+    $(document).on("facetrackingEvent", __bind(function(e) {
+      return this.log.register(e);
+    }, this));
+    $(document).on("zoomEvent", __bind(function(e, zoom) {
+      return this.zoom(zoom);
+    }, this));
+  }
+  TrackControll.prototype.zoom = function(diff) {
+    var current_size, new_size;
+    current_size = parseInt(this.article.css('font-size'));
+    new_size = current_size + (diff / 10);
+    new_size = Math.max(5, new_size);
+    return this.article.css('font-size', "" + new_size + "px");
+  };
+  return TrackControll;
+})();
+TrackLog = (function() {
+  function TrackLog() {
+    this.log = [];
+    this.size = 4;
+    this.logging_on = false;
+    $(document).on("click", __bind(function() {
+      return this.logging_on = !this.logging_on;
+    }, this));
+  }
+  TrackLog.prototype.register = function(event) {
+    var zoomDelta;
+    this.push(event);
+    zoomDelta = this.last().originalEvent.width - event.originalEvent.width;
+    if (zoomDelta !== 0) {
+      return $(document).trigger("zoomEvent", zoomDelta);
+    }
+  };
+  TrackLog.prototype.pure = function(event) {
+    return {
+      width: event.originalEvent.width,
+      height: event.originalEvent.height,
+      x: event.originalEvent.x,
+      y: event.originalEvent.y
+    };
+  };
+  TrackLog.prototype.push = function(event) {
+    this.log.push(event);
+    if (this.log.length > this.size) {
+      this.log.shift();
+    }
+    if (this.logging_on) {
+      return console.log(this.pure(event));
+    }
+  };
+  TrackLog.prototype.last = function() {
+    return this.log[0];
+  };
+  return TrackLog;
+})();
